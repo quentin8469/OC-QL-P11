321153dee63c1e173638173d705b513bc6528a0c
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-15 18:11:51 +0200

    add other test function for email testing

--- test/test_unitaires/test_email_error.py
+++ test/test_unitaires/test_email_error.py
@@ -4,15 +4,28 @@ server.app.config['TESTING'] = True
 client = server.app.test_client()
 
 
-def test_email_error():
+def test_unknow_email():
     """ """
     reponse = client.post('/showSummary',
                            data={'email': 'admin@bob.com'})
     assert reponse
 
 
-# def test_good_email():
-#     """"""
-#     reponse = client.post('/showSummary',
-#                            data={'email': 'john@simplylift.co'})
-#     assert reponse
+def test_empty_email():
+    """ """
+    reponse = client.post('/showSummary',
+                           data={'email': ''})
+    assert reponse
+
+
+def test_know_email():
+    """"""
+    reponse = client.post('/showSummary',
+                           data={'email': 'john@simplylift.co'})
+    assert reponse
+
+def test_not_a_email():
+    """"""
+    reponse = client.post('/showSummary',
+                           data={'email': '1585d456qef'})
+    assert reponse
