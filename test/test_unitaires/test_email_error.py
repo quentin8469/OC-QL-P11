b5efa8a4b9deaa8ea2a2edee44b23fcae4638ea4
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-15 16:07:26 +0200

    creat function test for good and bad mail

--- test/test_unitaires/test_email_error.py
+++ test/test_unitaires/test_email_error.py
@@ -1,3 +1,18 @@
+import server
+
+server.app.config['TESTING'] = True
+client = server.app.test_client()
+
+
 def test_email_error():
     """ """
-    pass
+    reponse = client.post('/showSummary',
+                           data={'email': 'admin@bob.com'})
+    assert reponse.status_code == 302
+
+
+def test_good_email():
+    """"""
+    reponse = client.post('/showSummary',
+                           data={'email': 'john@simplylift.co'})
+    assert reponse.status_code == 200
