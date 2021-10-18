a667d98e5e8aa8ff97cf000a7f0ab5bf19521cb4
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-18 20:05:50 +0200

    WIP on test functions

--- test/test_unitaires/test_booking_past_competition.py
+++ test/test_unitaires/test_booking_past_competition.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 import server
 
@@ -9,15 +10,30 @@ def client():
     return clients
 
 
-def test_new_competitions_dates():
+def test_new_competitions_dates(client):
     """
-    
+    check if the competition date is still valid
     """
-    pass
+    date = datetime.today()
+    date_competition = datetime.strptime(server.competitions[0]['date'], '%Y-%m-%d %H:%M:%S')
+    print(date)
+    print(date_competition)
+    if date_competition > date :
+        print(date)
+        print(date_competition)
+        assert date_competition == True
 
 
-def test_past_competitions_dates():
+def test_past_competitions_dates(client):
     """
+    check if the date of the competition is no longer valid
+    """
+    date = datetime.today()
+    date_competition = datetime.strptime(server.competitions[0]['date'], '%Y-%m-%d %H:%M:%S')
+    print(date)
+    print(date_competition)
+    if date_competition < date:
+        print(date)
+        print(date_competition)
+        assert date_competition == False
     
-    """
-    pass
