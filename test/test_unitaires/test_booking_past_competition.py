9cc97a344a913c5ee6a100e79fa8ab22df56294b
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-18 16:08:11 +0200

    WIP past competitions

---
+++ test/test_unitaires/test_booking_past_competition.py
@@ -0,0 +1,16 @@
+import pytest
+import server
+
+
+@pytest.fixture
+def client():
+    server.app.config['TESTING'] = True
+    clients = server.app.test_client()
+    return clients
+
+
+def test_competitions_dates():
+    """
+    
+    """
+    pass
