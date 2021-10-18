0476eb3ef1886bdec77ad982e4fe6f7d596496cb
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-19 00:12:13 +0200

    New test

--- test/test_unitaires/test_booking_past_competition.py
+++ test/test_unitaires/test_booking_past_competition.py
@@ -10,23 +10,47 @@ def client():
     return clients
 
 
+# def test_new_competitions_dates(client):
+#     """
+#     check if the competition date is still valid
+#     """
+#     date = datetime.today()
+#     date_competition = datetime.strptime(server.competitions[0]['date'], '%Y-%m-%d %H:%M:%S')
+#     if date_competition < date:
+#         today=True
+#         assert today
+    
+
+# def test_past_competitions_dates(client):
+#     """
+#     check if the date of the competition is no longer valid
+#     """
+#     date = datetime.today()
+#     date_competition = datetime.strptime(server.competitions[0]['date'], "%Y-%m-%d %H:%M:%S")
+#     if date_competition < date:
+#         today=False
+#         assert today
+
 def test_new_competitions_dates(client):
     """
     check if the competition date is still valid
     """
-    date = datetime.today()
-    date_competition = datetime.strptime(server.competitions[0]['date'], '%Y-%m-%d %H:%M:%S')
-    if date_competition < date:
-        today=True
-        assert today
-    
-
+    club = server.clubs[0]['name']
+    competition = server.competitions[1]
+    result = client.post('/purchasePlaces', data={'club': club,
+                                                  'competition': competition['name'],
+                                                  'numberOfPlaces': competition['numberOfPlaces'],
+                                                  'places': 5})
+    assert result.status_code == 200
+        
 def test_past_competitions_dates(client):
     """
     check if the date of the competition is no longer valid
     """
-    date = datetime.today()
-    date_competition = datetime.strptime(server.competitions[0]['date'], "%Y-%m-%d %H:%M:%S")
-    if date_competition < date:
-        today=False
-        assert today
+    club = server.clubs[0]['name']
+    competition = server.competitions[1]
+    result = client.post('/purchasePlaces', data={'club': club,
+                                                  'competition': competition['name'],
+                                                  'numberOfPlaces': competition['numberOfPlaces'],
+                                                  'places': 5})
+    assert result.status_code == 200
