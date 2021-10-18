2e18b034421cbaca7992ab073abb0086781373da
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-18 14:19:29 +0200

    WIP test update points

--- test/test_unitaires/test_update_points.py
+++ test/test_unitaires/test_update_points.py
@@ -11,7 +11,7 @@ def client():
 
 def test_good_update_points_after_reservation(client):
     """ """
-    club_points = server.clubs[0]['points']
+    club_points = int(server.clubs[0]['points'])
     print(club_points)
     reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 25})
     update_points = club_points
@@ -20,7 +20,7 @@ def test_good_update_points_after_reservation(client):
 
 def test_bad_update_points_after_reservation(client):
     """"""
-    club_points = 13
+    club_points = int(server.clubs[0]['points'])
     reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 25})
     update_points = club_points
     assert club_points == update_points
