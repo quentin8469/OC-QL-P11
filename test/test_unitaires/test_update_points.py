31686f1b8ccb5930964c80ccae1654dd7a495248
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-18 13:36:34 +0200

    WIP on test update points after reservation

--- test/test_unitaires/test_update_points.py
+++ test/test_unitaires/test_update_points.py
@@ -1,3 +1,18 @@
-def test_update_points():
+import server
+
+server.app.config['TESTING'] = True
+client = server.app.test_client()
+
+
+def test_good_update_points_after_reservation():
     """ """
-    pass
+    club_points = 1
+    reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 10})
+    
+    assert reponse
+
+def test_bad_update_points_after_reservation():
+    """"""
+    reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 1})
+    assert reponse
+
