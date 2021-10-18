36f6378b55eccac556edf89755099460469d81ed
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-18 14:33:46 +0200

    fix the update point test

--- test/test_unitaires/test_update_points.py
+++ test/test_unitaires/test_update_points.py
@@ -10,18 +10,35 @@ def client():
 
 
 def test_good_update_points_after_reservation(client):
-    """ """
+    """ 
+    Check if the update is good after the reservation.
+    Club_points need to be different of the update_points
+    """
     club_points = int(server.clubs[0]['points'])
-    print(club_points)
-    reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 25})
+    competition = server.competitions[0]
+    reponse = client.post('/purchasePlaces', 
+                          data={'club': 'Simply Lift', 
+                                'competition': competition['name'], 
+                                'numberOfPlaces': competition['numberOfPlaces']}
+                          )
     update_points = club_points
     assert club_points != update_points
 
 
+
 def test_bad_update_points_after_reservation(client):
-    """"""
+    """
+    Check if the update is bad after the reservation.
+    Club_points need to be different of the update_points.
+    if club points egal update points else we have a bad update
+    """
     club_points = int(server.clubs[0]['points'])
-    reponse = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'numberOfPlaces': 25})
+    competition = server.competitions[0]
+    reponse = client.post('/purchasePlaces', 
+                          data={'club': 'Simply Lift', 
+                                'competition': competition['name'], 
+                                'numberOfPlaces': competition['numberOfPlaces']}
+                          )
     update_points = club_points
     assert club_points == update_points
 
