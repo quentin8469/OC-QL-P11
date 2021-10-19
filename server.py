f8c4229739fa654a786266dbd96596fcaeffc93f
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-19 16:17:56 +0200

    Fix bug more than 12 places

--- server.py
+++ server.py
@@ -46,10 +46,12 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    
     if placesRequired > 12:
-        flash("You can't take more than 12 places")
+        competition['numberOfPlaces'] = competition['numberOfPlaces']
+        flash('You cant take more than 12 places')
     else:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
         flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
