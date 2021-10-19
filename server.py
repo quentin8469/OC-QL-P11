250ab5fae5f6c964e308e0a64ee49184340a3f1a
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-19 17:38:18 +0200

    fix bug take more than their points

--- server.py
+++ server.py
@@ -46,8 +46,12 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+    
+    if placesRequired > int(club['points']):
+        flash("Sorry, you don't have enough points")
+    else:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
