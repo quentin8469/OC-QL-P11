ceae798134d799330894c501437d5caffd1cad97
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-19 15:41:55 +0200

    WIP on test functions

--- test/test_unitaires/test_use_more_12_places.py
+++ test/test_unitaires/test_use_more_12_places.py
@@ -8,18 +8,42 @@ def client():
     clients = server.app.test_client()
     return clients
 
-def test_more_than_12_places():
+def test_more_than_12_places(client):
     """
     """
-    pass
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 12,
+                               })
+    assert result.status_code == 200
 
-def test_12_places():
+def test_12_places(client):
     """
     """
-    pass
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 12,
+                               })
+    assert result.status_code == 200
 
 
-def test_less_than_12_places():
+def test_less_than_12_places(client):
     """
     """
-    pass
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 3,
+                               })
+    assert result.status_code == 200
