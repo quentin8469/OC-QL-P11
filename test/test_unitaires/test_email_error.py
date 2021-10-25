77060c1e50b6b5bb25f56d206ead3373d171982d
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-25 14:59:39 +0200

    add test logout

--- test/test_unitaires/test_email_error.py
+++ test/test_unitaires/test_email_error.py
@@ -8,24 +8,29 @@ def test_unknow_email():
     """ """
     reponse = client.post('/showSummary',
                            data={'email': 'admin@bob.com'})
-    assert reponse
+    assert reponse.status_code == 302
 
 
 def test_empty_email():
     """ """
     reponse = client.post('/showSummary',
                            data={'email': ''})
-    assert reponse
+    assert reponse.status_code == 302
 
 
 def test_know_email():
     """"""
     reponse = client.post('/showSummary',
                            data={'email': 'john@simplylift.co'})
-    assert reponse
+    assert reponse.status_code == 200
 
 def test_not_a_email():
     """"""
     reponse = client.post('/showSummary',
                            data={'email': '1585d456qef'})
-    assert reponse
+    assert reponse.status_code == 302
+
+
+def test_logout():
+	reponse = client.get('/logout')
+	assert reponse.status_code == 302
