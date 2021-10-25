b650a24a1d4476f918b1210585e0f19b1d4ad7a3
Quentin Laparra <quentin.laparra@gmail.com>
2021-10-25 15:02:43 +0200

    add test for table displayclubspoints

--- test/test_unitaires/test_use_more_12_places.py
+++ test/test_unitaires/test_use_more_12_places.py
@@ -47,3 +47,10 @@ def test_less_than_12_places(client):
                                 'places': 3,
                                })
     assert result.status_code == 200
+    
+
+def test_clubs_table_points(client):
+	reponse = client.get('/displayclubsPoints')
+	assert reponse.status_code == 200
+	assert server.clubs[0]["name"] in reponse.data.decode()
+	assert server.clubs[1]["points"] in reponse.data.decode()
