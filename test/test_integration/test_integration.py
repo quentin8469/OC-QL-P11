b74a98d7a1376f886fc0d5b034fd265f91b80409
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-01 14:53:53 +0100

    PEP8

--- test/test_integration/test_integration.py
+++ test/test_integration/test_integration.py
@@ -1,34 +1,23 @@
-
-import pytest
-#from server import clubs
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 
-    
 
-
-#club = clubs[0]['email']
-
-
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
+service = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 driver.get('http://127.0.0.1:5000/')
 time.sleep(2)
-#email = driver.find_element_by_name('email').send_keys("john@simplylift.co")
 email = driver.find_element(By.NAME, 'email').send_keys("john@simplylift.co")
 driver.find_element(By.TAG_NAME,'button').click()
 time.sleep(2)
-#driver.find_elements_by_link_text('Book Places')[0].click()
 driver.find_element(By.ID,'book').click()
-#driver.find_elements_by_id('book')[0].click()
 time.sleep(2)
 places = driver.find_element(By.NAME,'places').send_keys('2')
-#places = driver.find_element_by_name('places').send_keys('2')
 time.sleep(2)
 driver.find_element(By.TAG_NAME,'button').click()
 time.sleep(2)
-#driver.find_elements_by_id('logout')[0].click()
 driver.find_elements(By.ID,'logout')[0].click()
 time.sleep(2)
 driver.quit()
