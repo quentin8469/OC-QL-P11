ae547c519c9f216094e275a12624421d512da64a
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-01 14:44:21 +0100

    Fix warning

--- test/test_integration/test_integration.py
+++ test/test_integration/test_integration.py
@@ -1,23 +1,34 @@
 
 import pytest
-#import server
+#from server import clubs
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-#club = server.clubs[0]['email']
+
+    
+
+
+#club = clubs[0]['email']
 
 
 driver = webdriver.Chrome(executable_path="chromedriver.exe")
 driver.get('http://127.0.0.1:5000/')
-email = driver.find_element_by_name('email')
-email.send_keys('john@simplylift.co')
-driver.find_element_by_tag_name('button').click()
 time.sleep(2)
-driver.find_elements_by_id('book')[0].click()
+#email = driver.find_element_by_name('email').send_keys("john@simplylift.co")
+email = driver.find_element(By.NAME, 'email').send_keys("john@simplylift.co")
+driver.find_element(By.TAG_NAME,'button').click()
 time.sleep(2)
-places = driver.find_element_by_name('places')
-places.send_keys('2')
+#driver.find_elements_by_link_text('Book Places')[0].click()
+driver.find_element(By.ID,'book').click()
+#driver.find_elements_by_id('book')[0].click()
 time.sleep(2)
-driver.find_element_by_tag_name('button').click()
+places = driver.find_element(By.NAME,'places').send_keys('2')
+#places = driver.find_element_by_name('places').send_keys('2')
+time.sleep(2)
+driver.find_element(By.TAG_NAME,'button').click()
+time.sleep(2)
+#driver.find_elements_by_id('logout')[0].click()
+driver.find_elements(By.ID,'logout')[0].click()
 time.sleep(2)
 driver.quit()
