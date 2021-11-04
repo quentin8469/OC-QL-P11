c4bae746a9239bb61cd2d8a486231bb20c51240b
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-05 00:06:38 +0100

    PEP8

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from server import loadClubs, loadCompetitions
 
 class HelloWorldUser(HttpUser):
     
-    wait_time = between(1, 5)
+    wait_time = between(1, 2)
     competitions = loadCompetitions()[0]['name']
     club = loadClubs()[0]["name"]
     email = loadClubs()[0]["email"] 
